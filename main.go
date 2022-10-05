c617bced44a50e06c2717671c2a94afb6627ff2a
brice <bricevadnais@yahoo.com>
2022-10-05 17:34:35 -0400

    blue: version: v0.0.18
    Missed go.mod and go.sum (probably caused issues with previous tag so
    creating a new one on this commit)

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 // VERSION is the version number of the blang repl and language
 // it will be incremented as seen fit
-const VERSION = "0.0.17"
+const VERSION = "0.0.18"
 
 func main() {
 	cmd.Run(VERSION, os.Args...)
