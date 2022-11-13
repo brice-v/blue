ce46e11a4ca64f078f45c6ad3494d83c0a293cd0
brice <bricevadnais@yahoo.com>
2022-11-13 18:31:30 -0500

    Updating Version to v0.1.2

--- consts/consts.go
+++ consts/consts.go
@@ -2,6 +2,6 @@ package consts
 
 // VERSION is the version number of the blang repl and language
 // it will be incremented as seen fit
-const VERSION = "0.1.0"
+const VERSION = "0.1.2"
 
 const PARSER_ERROR_PREFIX = "ParserError: "
