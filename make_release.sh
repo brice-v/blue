07ac2e897bc3428dab6613250f440b1402b679cb
brice <bricevadnais@yahoo.com>
2023-01-08 21:52:16 -0500

    add -race to make release script

--- make_release.sh
+++ make_release.sh
@@ -2,5 +2,5 @@
 set -o errexit
 set -o nounset
 
-go clean -testcache && go test ./... && go build -ldflags='-s -w' && strip blue && upx blue && cp blue ~/.blue/bin/
+go clean -testcache && go test -race ./... && go build -ldflags='-s -w' && strip blue && upx blue && cp blue ~/.blue/bin/
 
