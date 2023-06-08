765d7d69e411b9498b1e2eec3fb7417f01cea31a
brice <bricevadnais@yahoo.com>
2023-06-07 20:30:57 -0400

    release script: update to set BLUE_INSTALL_PATH for bundler

--- make_release.bat
+++ make_release.bat
@@ -1,1 +1,1 @@
-go clean -testcache && go test ./... && go build -ldflags="-s -w -extldflags='-static'" && strip blue.exe && upx --best blue.exe && cp blue.exe C:/Users/brice/OneDrive/Documents/.blue/bin/
+go clean -testcache && go test ./... && go build -ldflags="-s -w -extldflags='-static'" && strip blue.exe && upx --best blue.exe && cp blue.exe C:/Users/brice/OneDrive/Documents/.blue/bin/ && set BLUE_INSTALL_PATH=C:/Users/brice/OneDrive/Documents/.blue/src/
