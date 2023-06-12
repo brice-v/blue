e3b7667095a4df92e751a95f75efdc382de333eb
Brice Vadnais <bricevadnais@yahoo.com>
2023-06-11 21:07:56 -0400

    v0.1.10 Update Version and TODOs

--- consts/consts.go
+++ consts/consts.go
@@ -9,7 +9,7 @@ import (
 // VERSION is the version number of the blang repl and language
 // it will be incremented as seen fit
 var VERSION = func() string {
-	version := "0.1.9"
+	version := "0.1.10"
 	hash := ""
 	os := ""
 	arch := ""
