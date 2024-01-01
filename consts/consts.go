4a8a2366ce657fc870b46de22ec88a6134e4a521
Brice Vadnais <bricevadnais@yahoo.com>
2024-01-01 14:21:24 -0500

    v0.1.17

--- consts/consts.go
+++ consts/consts.go
@@ -10,7 +10,7 @@ import (
 // VERSION is the version number of the blang repl and language
 // it will be incremented as seen fit
 var VERSION = func() string {
-	version := "0.1.16"
+	version := "0.1.17"
 	hash := ""
 	os := ""
 	arch := ""
