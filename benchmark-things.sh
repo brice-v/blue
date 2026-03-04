13e4060585d6337aeee988ec5528c33c4ba7795b
brice <bricevadnais@yahoo.com>
2026-03-04 10:47:05 -0500

    Update Latest Benchmark

--- benchmark-things.sh
+++ benchmark-things.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-hyperfine -n blue-baseline "./blue manual_tests/fib-ex.b" \
- -n blue-vm "./blue --vm manual_tests/fib-ex.b" \
+hyperfine -n blue-v0.2.3 "./blue-v0.2.3 manual_tests/fib-ex.b" \
+ -n blue-v0.3.0 "./blue manual_tests/fib-ex.b --vm" \
  --runs 10 --export-markdown  hf.md && cat hf.md
