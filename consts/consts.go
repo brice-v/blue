64a3b95ca039ac115606bb0d7cd7babd9816b62c
brice <bricevadnais@yahoo.com>
2026-03-15 21:48:49 -0400

    v0.3.2
    Updated Mac to build for intel as well

--- consts/consts.go
+++ consts/consts.go
@@ -8,7 +8,7 @@ import (
 )
 
 // versionNumber is the current version #
-const versionNumber = "0.3.1"
+const versionNumber = "0.3.2"
 
 // versionFn is the function that returns the formatted version number of the blang repl and language
 func versionFn() string {
